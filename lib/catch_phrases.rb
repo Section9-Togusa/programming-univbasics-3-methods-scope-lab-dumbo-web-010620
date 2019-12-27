e595ee8189b663c2c667ae7d35abb4b9f7b1c741
Daryl Corbin <corbindac@gmail.com>
2019-12-27 21:46:44 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,15 +1,16 @@
-$status = 'Thank You Mario! But Our Princess Is In Another Castle!'
+
 def mario
   phrase = "It's-a me, Mario!"
   puts phrase
 end
 
 def toadstool
+  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   puts status
 end
 
 def link
-  phrase = "It's Dangerous To Go Alone"
+  phrase = "It's Dangerous To Go Alone! Take This"
   puts phrase
 end
 
